b9faa1f590463bb204214b01861a6c046388b935
KirillZavgorodniy <Kirill.Zavgorodniy@gmail.com>
2020-08-12 03:30:55 +0300

    Validation was added

--- source/js/form.js
+++ source/js/form.js
@@ -3,6 +3,7 @@
     var form = document.querySelector('.formContainer');
     var closeButton = null;
 
+
     //Закрытие по Esc???
     
     function onClose(e) {
@@ -26,6 +27,37 @@
         form.classList.add('isHidden');
     };
 
+    me.isValid = function() {
+        var requiredFildsValue = document.querySelectorAll('[data-valid="required"]');
+        var emailValue = document.querySelector('[data-email]').value;
+        var numberValue = document.querySelector('[data-number]').value;
 
-    window.form = me;
+        if (!me.isAllComplited()) {
+            console.log('Write in all fields');
+        } else if (!BusinessTGH.validation.isEmail(emailValue)){
+            console.log('Invalid E-mail');
+            return false;
+        } else if (!BusinessTGH.validation.isNumber(numberValue)){
+            console.log('Invalid number');
+            return false;
+        } else  {
+            return true;
+        }
+    };
+
+    me.isAllComplited = function(data) {
+        var result = true;
+
+        for (var i = 0; i < data.length; i++) {
+            if (!BusinessTGH.validation.isNotEmpty(data[i].value)) {
+                result = false;
+                break
+            };
+        };
+
+        return result;
+    };
+
+
+    BusinessTGH.form = me;
 }());
--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,29 @@
 (function() {
 
     var openFormButton = document.querySelector('.arrowDown');
+    var form = document.querySelector('.form');
 
     openFormButton.addEventListener('click', function() {
-        form.open();
+        BusinessTGH.form.open();
     });
 
     if (openFormButton) {
         openFormButton.addEventListener('click', function(e) {
             e.preventDefault;
-            form.open();
+            BusinessTGH.form.open();
         });
+    };
+
+    if (form) {
+        form.addEventListener('click', function(e) {
+            e.preventDefault();
+            if (BusinessTGH.form.isValid()) {
+                console.log('all good');
+            } else {
+                console.log('is not valid');
+            }
+            
+        })
     };
 
     console.log(openFormButton);
